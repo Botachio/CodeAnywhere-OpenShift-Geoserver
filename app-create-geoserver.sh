054cbd7932e4ce6826000ae9727daf4a7da3cab8
jeroen <jeroen@tweejee.net>
2016-02-03 16:02:45 -0500

    first experiment with action_hooks

--- app-create-geoserver.sh
+++ app-create-geoserver.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-rhc app create geoserver tomcat7 --gear-size medium --region aws-eu-west-1
-cd geoserver
+rhc app create geoserver tomcat7 --gear-size medium --region aws-eu-west-1 --repo geoserver-repo
+cd geoserver-repo
 rm pom.xml
 wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.8.2/geoserver-2.8.2-war.zip
 unzip geoserver-2.8.2-war.zip
 rm geoserver-2.8.2-war.zip
 unzip geoserver.war -d webapps\geoserver
 rm geoserver.war
+cp -r ../.openshift .
+
