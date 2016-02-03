5691eee7d2d01dabb56a43845a172d6fecfa864e
jeroen <jeroen@tweejee.net>
2016-02-03 16:50:00 -0500

    typo in path

--- app-create-geoserver.sh
+++ app-create-geoserver.sh
@@ -6,7 +6,7 @@ rm pom.xml
 wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.8.2/geoserver-2.8.2-war.zip
 unzip geoserver-2.8.2-war.zip
 rm geoserver-2.8.2-war.zip
-unzip geoserver.war -d webapps\geoserver
+unzip geoserver.war -d webapps/geoserver
 rm geoserver.war
 cp --recursive --preserve=mode ../.openshift .
 git add -A .
