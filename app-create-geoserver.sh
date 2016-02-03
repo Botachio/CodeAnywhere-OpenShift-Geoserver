61f63d7d5b1e0cf01fd08d85da6c4f8e4a0acce5
jeroen <jeroen@tweejee.net>
2016-02-03 15:19:18 -0500

    extended script to download and extract geoserver

--- app-create-geoserver.sh
+++ app-create-geoserver.sh
@@ -2,3 +2,9 @@
 
 rhc app create geoserver tomcat7 --gear-size medium --region aws-eu-west-1
 cd geoserver
+rm pom.xml
+wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.8.2/geoserver-2.8.2-war.zip
+unzip geoserver-2.8.2-war.zip
+rm geoserver-2.8.2-war.zip
+unzip geoserver.war -d webapps\geoserver
+rm geoserver.war
