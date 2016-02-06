b878cbcf3a607e68f03e6f9fa20275b4349298aa
jeroen <jeroen@tweejee.net>
2016-02-06 14:26:12 -0500

    first working scaling app

--- app-create-geoserver.sh
+++ app-create-geoserver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rhc app create geoserver tomcat7 --gear-size medium --region aws-eu-west-1 --repo geoserver-repo
+rhc app create geoserver tomcat7 postgresql-9 --scaling --gear-size small.highcpu --region aws-eu-west-1 --repo geoserver-repo
+rhc cartridge scale tomcat7 --app geoserver --max 1    
 cd geoserver-repo
 rm pom.xml
 wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.8.2/geoserver-2.8.2-war.zip
